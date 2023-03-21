dccf5ae2832be1c6d4565d6351e7860bcabecae6
LincolnMuraguri <lincolnmuraguri93@gmail.com>
2023-03-21 15:12:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+const cards = ['Guadalupe','Ollie','Aki'];
+function writeCards(cards, event='surprise') {
+    const messages = []
+    for (let i = 0; i < cards.length; i++) {
+    const message = `Thank you, ${cards[i]}, for the wonderful ${event} gift!`;
+    messages.push(message);
+    }
+    console.log(messages)
+    return messages;
+}
+
+function countDown(count) {
+    let message1 = []
+    while (count >= 0) {
+        message1.push(count);
+        console.log(count)
+        count = count-1;
+    }
+    return message1
+    
+}
+console.log(countDown(4))
